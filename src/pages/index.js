faf2f4bcfed8fe665293be2cac64f857fbd75bbe
samuelcpr <samuelcpr464@gmail.com>
2023-07-06 11:14:08 -0300

    feat: adicionando primeiro item (image) em portifolho

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+
+import MyImage from "../components/imagens/image"
+
+
+
+export default () => {
+	return (
+		<div className="main">
+
+			<header>
+				<div>
+					<MyImage/>
+				</div>
+			</header>
+
+
+		</div>
+	)
+}
+
